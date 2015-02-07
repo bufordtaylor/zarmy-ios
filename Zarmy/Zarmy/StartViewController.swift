3d4cc59f67cf1c2911afe6ee42f3c9ccea3db760
Adam Clarke <adam@adamclarke.co>
2015-02-07 10:16:50 +0000

    Improved Start screen layout for all devices

--- Zarmy/Zarmy/StartViewController.swift
+++ Zarmy/Zarmy/StartViewController.swift
@@ -14,12 +14,22 @@ class StartViewController: UIViewController {
   @IBOutlet var loginButton: UIButton!
   @IBOutlet var signupButton: UIButton!
   
+  @IBOutlet var toddlerWidth: NSLayoutConstraint!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
     facebookButton.layer.cornerRadius = 3
     facebookButton.backgroundColor = UIColor(red: 59.0/255.0, green: 89.0/255.0, blue: 152.0/255.0, alpha: 1.0)
-
     
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    
+    NSLog("frame.width = \(view.frame.size.width)")
+
+    if view.frame.size.width < 350 {
+      toddlerWidth.constant = 120.0;
+    }
   }
 }
